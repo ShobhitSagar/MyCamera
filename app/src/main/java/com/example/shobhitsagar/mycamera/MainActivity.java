72c639e096c483bbe35466c43a0733f38e31e2ee
ShobhitSagar <shobhitsagar109@gmail.com>
2021-06-12 00:53:16 +0530

    first commit

---
+++ app/src/main/java/com/example/shobhitsagar/mycamera/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.shobhitsagar.mycamera;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    ImageView imageViewbtn;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageViewbtn = (ImageView) findViewById(R.id.imageViewbtn);
+
+    }
+
+    public void dispatchTakePictureIntent(View view){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestcode, int resultcode, Intent data) {
+        if (requestcode == REQUEST_IMAGE_CAPTURE && resultcode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+}
